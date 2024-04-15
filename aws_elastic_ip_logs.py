1b51c2c83ec295a9e48b732020905fde3bd229b2
zeadailson17 <149551798+zeadailson17@users.noreply.github.com>
2024-04-15 11:33:57 -0300

    Update get the route53 events

--- aws_elastic_ip_logs.py
+++ aws_elastic_ip_logs.py
@@ -9,46 +9,58 @@ def lambda_handler(event, context):
     log_event(event, context)
 
     try:
-        event_source = event['source']
-        detail_type = event['detail-type']
-        event_time = event['time']
-        event_name = event['detail']['eventName']
-        event_region = event['region']
-        user_identity = event['detail']['userIdentity']['principalId'].split(':')[1]
+        source = event.get('source')
+        detail_type = event.get('detail-type')
+        event_time = event.get('time')
+        detail = event.get('detail', {})
+        user_identity = detail.get('userIdentity', {}).get('principalId', '').split(':')[1]
 
         instance_id_or_public_ip_or_dns_name = ''
 
-        if 'detail' in event and 'responseElements' in event['detail']:
-            response_elements = event['detail']['responseElements']
-            if 'instancesSet' in response_elements:
+        if 'responseElements' in detail or 'requestParameters' in detail:
+            response_elements = detail.get('responseElements', {})
+            request_parameters = detail.get('requestParameters', {})
+            
+            if 'changeBatch' in request_parameters:
+                route53_action = request_parameters['changeBatch']['changes'][0]['action']
+                resource_record_set = request_parameters['changeBatch']['changes'][0]['resourceRecordSet']['name']
+                message = f"Cria registro de DNS no Route53: {source} {detail_type}\n" \
+                          f"Event_name: {detail.get('eventName')}\n" \
+                          f"User: {user_identity}\n" \
+                          f"Route53 Event: {route53_action}\n" \
+                          f"Name: {resource_record_set}\n" \
+                          f"Region: {event.get('region')}\n" \
+                          f"Time: {event_time}"
+                instance_id_or_public_ip_or_dns_name = f"Route53 Record Name: {resource_record_set}"
+            elif 'instancesSet' in response_elements:
                 instance_id = response_elements['instancesSet']['items'][0]['instanceId']
-                message = f"Received EC2 instance creation event: {event_source} {detail_type}\n" \
-                          f"Event_name: {event_name}\n" \
+                message = f"Received EC2 instance creation event: {source} {detail_type}\n" \
+                          f"Event_name: {detail.get('eventName')}\n" \
                           f"User: {user_identity}\n" \
                           f"Instance ID: {instance_id}\n" \
-                          f"Region: {event_region}\n" \
+                          f"Region: {event.get('region')}\n" \
                           f"Time: {event_time}"
                 instance_id_or_public_ip_or_dns_name = f"Instance ID: {instance_id}"
             elif 'publicIp' in response_elements:
                 public_ip = response_elements['publicIp']
-                message = f"Received Elastic IP allocation event: {event_source} {detail_type}\n" \
-                          f"Event_name: {event_name}\n" \
+                message = f"Received Elastic IP allocation event: {source} {detail_type}\n" \
+                          f"Event_name: {detail.get('eventName')}\n" \
                           f"User: {user_identity}\n" \
                           f"Public IP: {public_ip}\n" \
-                          f"Region: {event_region}\n" \
+                          f"Region: {event.get('region')}\n" \
                           f"Time: {event_time}"
                 instance_id_or_public_ip_or_dns_name = f"Elastic IP: {public_ip}"
             elif 'loadBalancers' in response_elements:
                 load_balancers = response_elements['loadBalancers']
                 if load_balancers:
                     dns_name = load_balancers[0]['dNSName']
-                    message = f"Received ELB creation event creation event: {event_source} {detail_type}\n" \
-                              f"Event_name: {event_name}\n" \
+                    message = f"Received ELB creation event: {source} {detail_type}\n" \
+                              f"Event_name: {detail.get('eventName')}\n" \
                               f"User: {user_identity}\n" \
                               f"Load Balancer DNS: {dns_name}\n" \
-                              f"Region: {event_region}\n" \
+                              f"Region: {event.get('region')}\n" \
                               f"Time: {event_time}"
-                    instance_id_or_public_ip_or_dns_name = f"ELB Event: {event_name}"
+                    instance_id_or_public_ip_or_dns_name = f"ELB Event: {detail.get('eventName')}"
                 else:
                     print("No load balancers found in the event.")
                     return
@@ -56,17 +68,16 @@ def lambda_handler(event, context):
                 print("Unsupported event or no response elements found in the event.")
                 return
 
-            subject=f"AWS Notification: {instance_id_or_public_ip_or_dns_name} - User: {user_identity} - {event_region}" #The official AWS resources that confirm the 100-character limit for SNS subject lines
+            subject = f"AWS Notification: {instance_id_or_public_ip_or_dns_name} - User: {user_identity} - {event.get('region')}"
 
             print(message)
             sns_client.publish(
-                TopicArn='arn:aws:sns:us-east-1:123456789012:PublicIPNotificationTopic',
+                TopicArn='arn:aws:sns:sa-east-1:123456789012:PublicIPNotificationTopic',
                 Message=message,
-                Subject=subject
+                Subject=subject[:100]  # Ensure subject is within 100 characters
             )
         else:
-            print("No 'detail' key or 'responseElements' key found in the event.")
-            return
+            print("No 'responseElements' or 'requestParameters' key found in the event.")
     except KeyError as e:
         print(f"Error: {e}")
         print(f"Event: {event}")
